d385da16878e434a99868c1917607c530dcb4dbc
Mathu28 <150362420+Mathu28@users.noreply.github.com>
2026-02-02 22:30:15 +0530

    added git ignorre

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from service.cloudtrial_processing import process_cloudtrail_csv
+
+app = FastAPI(title="CloudTrail Log Processing API")
+
+@app.get("/process-cloudtrail")
+def process_cloudtrail():
+    bucket_name = "cloudtrail-log-details-storage-bucket"
+    file_key = "archive/dec12_18features.csv"
+
+    processed, sentences = process_cloudtrail_csv(
+        bucket_name,
+        file_key
+    )
+
+    return {
+        "status": "success",
+        "processed_logs": processed,
+        "sample_output": sentences[:5]
+    }
